e1d53e6d9ce19d67e95e21b29093d32b0ff772d2
Lapoozza <jewelmahanta@gmail.com>
2017-04-29 22:36:26 +0530

    ref (database  init): added a better way to initialize
    Now database can be initialized and used by simply calling, for example:
    Database(db_type="sqlite3"). This was achieved by inheriting the SQLite3Database as
    the base class.

--- web/Bahon/modules/database/__init__.py
+++ web/Bahon/modules/database/__init__.py
@@ -19,18 +19,25 @@ along with Bahon.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from os import path
-from Bahon.modules.database import sqlite3_database
+from Bahon.modules.database.sqlite3_database import SQLite3Database
 
 defaults = {
     "sqlite3_path": path.abspath("./Bahon/extras/test.db")
 }
 
 
-class Database:
+class Database(SQLite3Database):
     """
     This class manages the various databases that can be used with Bahon.
     For now only sqlite3 is supported.
     """
+
+    # TODO: add a way to test if database is initialized with records and stuff
+    # TODO: the path from kwargs should also be absolute
     def __init__(self, db_type: str, **kwargs):
+
+        # since we are going to do multiple inheritance
+        # using super to initialize the base classes is
+        # not a very good idea.
         if db_type == "sqlite3":
-            self.database = sqlite3_database.Database(kwargs.get("sqlite3_path") or defaults["sqlite3_path"])
+            SQLite3Database.__init__(self, kwargs.get("sqlite3_path") or defaults["sqlite3_path"])
